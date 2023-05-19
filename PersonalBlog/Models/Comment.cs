da9044c6daa5d238fa9cfbf10f5d3f33c1b1c701
AAARRRRR <zhongjye@126.com>
2023-05-19 09:23:10 +0800

    [fix] fix layout issue

--- PersonalBlog/Models/Comment.cs
+++ PersonalBlog/Models/Comment.cs
@@ -1,10 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace PersonalBlog.Models;
 
 public class Comment
 {
     public int Id { get; set; }
+    [Required]
     public string Name { get; set; }
     public string? Contact { get; set; }
+    [Required]
+    
     public string Content { get; set; }
     public bool IsDisplay { get; set; }
     public DateTime CreatedDate { get; set; }
