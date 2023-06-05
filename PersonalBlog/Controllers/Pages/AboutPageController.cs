5b35c3d9f92cfb5007fc321f5dbe1fa2dc365069
AAARRRRR <zhongjye@126.com>
2023-06-05 15:25:16 +0800

    [feat] add about page controller

---
+++ PersonalBlog/Controllers/Pages/AboutPageController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PersonalBlog.Controllers;
+
+[AllowAnonymous]
+public class AboutPageController : Controller
+{
+    
+}
